9e4cfd647972a5278b9558f8f34d2655b5efebc3
Jacob Schlecht <jschlecht@illfonic.com>
2022-05-18 23:15:51 -0600

    Delete vendor folder

--- vendor/gioui.org/io/key/mod_darwin.go
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Unlicense OR MIT
-
-package key
-
-// ModShortcut is the platform's shortcut modifier, usually the Ctrl
-// key. On Apple platforms it is the Cmd key.
-const ModShortcut = ModCommand
